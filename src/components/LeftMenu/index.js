2c8e1b136235116e1bd226802bf9166bfedc1d03
xuaoneto <60081611+xuaoneto@users.noreply.github.com>
2021-09-19 16:56:42 -0300

    WIP: internal-menu

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -1,9 +1,53 @@
 import React from "react";
+import InsertSVG from "../InsertSVG";
 
 const LeftMenu = ({ state }) => {
+  const rightarrow = <InsertSVG name="rightarrowicon" />;
+  const internalmenu = [
+    { name: "HOME", icon: rightarrow },
+    { name: "PORTFOLIO", icon: rightarrow },
+    { name: "SERVICES", icon: rightarrow },
+    { name: "CONTACTS", icon: rightarrow },
+    { name: "BLOG", icon: rightarrow },
+    { name: "PAGES", icon: rightarrow },
+  ];
+
+  const ActiveItem = (id) => {
+    internalmenu.map((item, index) => {
+      let active = document.getElementsByClassName("item-name");
+      if (item.name === id) {
+        active[index].className = "item-name active";
+      } else {
+        active[index].className = "item-name";
+      }
+    });
+  };
+
   return (
     <div className={state ? "leftmenu-container active" : "leftmenu-container"}>
-      <div></div>
+      <div className="pesonal-name">
+        <a className="name" href="#">
+          Ezequias
+        </a>
+        <div className="hovered"></div>
+      </div>
+      <div className="internalmenu-container">
+        {internalmenu.map((item) => {
+          return (
+            <div
+              className="menu-item"
+              id={`${item.name}-menu`}
+              onClick={() => {
+                ActiveItem(item.name);
+              }}
+            >
+              <div className="hovered"></div>
+              <p className="item-name">{item.name}</p>
+              {rightarrow}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
